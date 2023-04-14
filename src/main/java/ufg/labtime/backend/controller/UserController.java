4ec29d54574b426d0c3a8487393e6bca4e1f3178
Mesquita <mesquita@aospa.co>
2023-04-14 12:15:35 -0300

    controller: allow using filters
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- src/main/java/ufg/labtime/backend/controller/UserController.java
+++ src/main/java/ufg/labtime/backend/controller/UserController.java
@@ -1,17 +1,16 @@
 package ufg.labtime.backend.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ufg.labtime.backend.entity.Usuario;
 import ufg.labtime.backend.repository.UserRepository;
 import ufg.labtime.backend.utils.CSVUtils;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -23,7 +22,15 @@ public class UserController {
     private final String UPDATE_ERROR = "Erro ao atualizar base de dados";
 
     @RequestMapping(value = "/usuarios", method = RequestMethod.GET)
-    public List<Usuario> Get() {
+    public List<Usuario> Get(@RequestParam Map<String, String> queryParameters) {
+        if (MapUtils.isNotEmpty(queryParameters)) {
+            if (!userRepository.findByFullNameContaining(queryParameters.get("filtro")).isEmpty())
+                return userRepository.findByFullNameContaining(queryParameters.get("filtro"));
+            if (!userRepository.findBySocialNameContaining(queryParameters.get("filtro")).isEmpty())
+                return userRepository.findBySocialNameContaining(queryParameters.get("filtro"));
+            if (!userRepository.findByEmailContaining(queryParameters.get("filtro")).isEmpty())
+                return userRepository.findByEmailContaining(queryParameters.get("filtro"));
+        }
         return userRepository.findAll();
     }
 
--- src/main/java/ufg/labtime/backend/repository/UserRepository.java
+++ src/main/java/ufg/labtime/backend/repository/UserRepository.java
@@ -4,6 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ufg.labtime.backend.entity.Usuario;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<Usuario, Long> {
+
+    List<Usuario> findByFullNameContaining(String fullName);
+
+    List<Usuario> findBySocialNameContaining(String socialName);
+
+    List<Usuario> findByEmailContaining(String email);
 }
