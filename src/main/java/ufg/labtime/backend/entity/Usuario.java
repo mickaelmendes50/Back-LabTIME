5c587be28a1ef9bfcc81c5f9ee2bbd00fef30d1f
Mesquita <mesquita@aospa.co>
2023-04-17 10:04:11 -0300

    Usuario: Remove CSV flags from class
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- src/main/java/ufg/labtime/backend/entity/Usuario.java
+++ src/main/java/ufg/labtime/backend/entity/Usuario.java
@@ -1,6 +1,5 @@
 package ufg.labtime.backend.entity;
 
-import com.opencsv.bean.CsvBindByPosition;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -13,71 +12,60 @@ public class Usuario {
     @Id
     @Getter
     @Setter
-    @CsvBindByPosition(position = 3)
     private String code;
 
     @Column(nullable = false)
     @NotEmpty(message = "É necessário informar o nome completo")
     @Getter
     @Setter
-    @CsvBindByPosition(position = 0)
     private String fullName;
 
     @Column(nullable = false)
     @NotEmpty(message = "É necessário informar o nome social")
     @Getter
     @Setter
-    @CsvBindByPosition(position = 1)
     private String socialName;
 
     @Column(nullable = true)
     @Getter
     @Setter
-    @CsvBindByPosition(position = 2)
     private String birthDate;
 
     @Column(nullable = true)
     @Getter
     @Setter
-    @CsvBindByPosition(position = 4)
     private String sex;
 
     @Column(nullable = false)
     @NotEmpty(message = "É necessário informar o email")
     @Getter
     @Setter
-    @CsvBindByPosition(position = 5)
     private String email;
 
     @Column(nullable = true)
     @Getter
     @Setter
-    @CsvBindByPosition(position = 6)
     private String state;
 
     @Column(nullable = true)
     @Getter
     @Setter
-    @CsvBindByPosition(position = 7)
     private String city;
 
     @Column(nullable = true)
     @Getter
     @Setter
-    @CsvBindByPosition(position = 9)
     private String situation;
 
     @Column(nullable = false)
     @NotEmpty(message = "É necessário informar a data de vinculo")
     @Getter
     @Setter
-    @CsvBindByPosition(position = 10)
     private String bondDate;
 
     @Column(nullable = false)
     @NotNull(message = "É necessário informar o numero de acesso")
     @Getter
     @Setter
-    @CsvBindByPosition(position = 8)
     private String accessNumber;
 }
