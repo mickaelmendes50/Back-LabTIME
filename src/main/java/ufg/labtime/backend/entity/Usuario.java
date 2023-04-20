5b7683fe9681c406abf46ee65ef3469cdc9d2c54
Mesquita <mesquita@aospa.co>
2023-04-20 14:54:47 -0300

    Usuario: Override getters and setters annotations with data annotation
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- src/main/java/ufg/labtime/backend/entity/Usuario.java
+++ src/main/java/ufg/labtime/backend/entity/Usuario.java
@@ -3,69 +3,47 @@ package ufg.labtime.backend.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
+@Data
 @Entity
 public class Usuario {
 
     @Column(nullable = false)
     @NotEmpty(message = "É necessário informar o nome completo")
-    @Getter
-    @Setter
     private String nomeCompleto;
 
     @Column(nullable = false)
     @NotEmpty(message = "É necessário informar o nome social")
-    @Getter
-    @Setter
     private String nomeSocial;
 
     @Column(nullable = true)
-    @Getter
-    @Setter
     private String dataDeNascimento;
 
     @Id
-    @Getter
-    @Setter
     private int codigo;
 
     @Column(nullable = true)
-    @Getter
-    @Setter
     private String sexo;
 
     @Column(nullable = false)
     @NotEmpty(message = "É necessário informar o email")
-    @Getter
-    @Setter
     private String email;
 
     @Column(nullable = true)
-    @Getter
-    @Setter
     private String estado;
 
     @Column(nullable = true)
-    @Getter
-    @Setter
     private String municipio;
 
     @Column(nullable = false)
     @NotNull(message = "É necessário informar o numero de acesso")
-    @Getter
-    @Setter
     private int numeroDeAcessos;
 
     @Column(nullable = true)
-    @Getter
-    @Setter
     private String situacao;
 
     @Column(nullable = false)
     @NotEmpty(message = "É necessário informar a data de vinculo")
-    @Getter
-    @Setter
     private String dataDeVinculo;
 }
