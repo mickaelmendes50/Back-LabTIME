0f81f04c99ec944976a282acfda6c446a9937af9
Mesquita <mesquita@aospa.co>
2023-04-20 15:22:41 -0300

    Usuario: Hide column annotation when available
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- src/main/java/ufg/labtime/backend/entity/Usuario.java
+++ src/main/java/ufg/labtime/backend/entity/Usuario.java
@@ -16,31 +16,21 @@ public class Usuario {
     @Column(nullable = false)
     @NotEmpty(message = "É necessário informar o nome social")
     private String nomeSocial;
-
-    @Column()
     private String dataDeNascimento;
 
     @Id
     private int codigo;
-
-    @Column()
     private String sexo;
 
     @Column(nullable = false)
     @NotEmpty(message = "É necessário informar o email")
     private String email;
-
-    @Column()
     private String estado;
-
-    @Column()
     private String municipio;
 
     @Column(nullable = false)
     @NotNull(message = "É necessário informar o numero de acesso")
     private int numeroDeAcessos;
-
-    @Column()
     private String situacao;
 
     @Column(nullable = false)
