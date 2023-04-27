7f986e0bddf61e98d8f53a613bb56c4b27bb78ea
Mesquita <mesquita@aospa.co>
2023-04-27 14:49:12 -0300

    index: setup server and root endpoint
    Signed-off-by: Mesquita <mesquita@aospa.co>

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import { Router, Request, Response } from 'express';
+
+const app = express();
+const route = Router()
+
+app.use(express.json())
+route.get('/', (req: Request, res: Response) => {
+    res.json({ info: 'API de Usuários' })
+})
+app.use(route)
+
+app.listen(3000, () => 'server running on port 3000')
